18f2ebed976aac9b513c6fe8a05118d0579ccdea
LakshyaVerma123kl <lakshya123kl@gmail.com>
2024-09-16 12:18:22 +0530

    new4

--- project/src/App.jsx
+++ project/src/App.jsx
@@ -4,11 +4,11 @@ import Header from "./Header";
 import Footer from "./Footer";
 import MainPage from "./MainPage";
 import Doctor from "./Doctor";
-import Signin from "./authentication/mainsignin";  // From branch2
-import Signup from "./authentication/mainsignup";  // From branch2
-import Issues from "./Issues";                     // From master
-import Resource from "./Resource";                 // From master
-import Mental from "./Mental";                     // From master
+import Signin from "./authentication/mainsignin"; // From branch2
+import Signup from "./authentication/mainsignup"; // From branch2
+import Issues from "./Issues"; // From master
+import Resource from "./Resource"; // From master
+import Mental from "./Mental"; // From master
 
 function App() {
   return (
@@ -17,11 +17,12 @@ function App() {
       <Routes>
         <Route path="/" element={<MainPage />} />
         <Route path="/doctor" element={<Doctor />} />
-        <Route path="/login" element={<Signin />} />    {/* From branch2 */}
-        <Route path="/signup" element={<Signup />} />   {/* From branch2 */}
-        <Route path="/issues" element={<Issues />} />   {/* From master */}
-        <Route path="/resource" element={<Resource />} />{/* From master */}
-        <Route path="/mental" element={<Mental />} />   {/* From master */}
+        <Route path="/login" element={<Signin />} /> {/* From branch2 */}
+        <Route path="/signup" element={<Signup />} /> {/* From branch2 */}
+        <Route path="/issues" element={<Issues />} /> {/* From master */}
+        <Route path="/resource" element={<Resource />} />
+        {/* From master */}
+        <Route path="/mental" element={<Mental />} /> {/* From master */}
       </Routes>
       <Footer />
     </Router>
--- project/src/authentication/mainsignup.jsx
+++ project/src/authentication/mainsignup.jsx
@@ -4,8 +4,9 @@ const Signup = () => {
   return (
     <div className="flex flex-wrap w-full h-full max-sm:space-y-0">
       <div className="w-[45%] max-sm:hidden text-left h-full p-10 bg-[url('./assets/photo.avif')] bg-cover bg-no-repeat">
-        <h1 className="font-[Forte] text-[1.8rem] mb-8 font-light">e-comm</h1>
+        <h1 className="font-[Forte] text-[1.8rem] mb-8 font-light">Zen-Men</h1>
       </div>
+
       <div className="flex justify-end w-[55%] max-sm:w-full max-sm:h-auto h-full px-5 py-5">
         <SignUptemplate />
       </div>
